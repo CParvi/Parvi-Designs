8858727834fbadadb9b1a6905c301fae68a17636
CParvi <77135493+CParvi@users.noreply.github.com>
2021-01-30 18:09:19 -0800

    Update offerexpires.js

--- Javascript/offerexpires.js
+++ Javascript/offerexpires.js
@@ -3,10 +3,10 @@
 //PART ONE: CREATE HOTEL OBJECT AND WRITE OUT THE OFFER DETAILS
 
 //Create a hotel object using object literal syntax
-var hotel = {
-name: 'Park',
-roomRate: 240, // Amount in dollars
-discount: 15, // Percentage discount
+var commission = {
+name: 'Get one commission of your choice 20% off!',
+roomRate: 50, // Amount in dollars
+discount: 20, // Percentage discount
 offerPrice: function() {
 var offerRate = this.roomRate * ((100 - this.discount) / 100);
 return offerRate;
@@ -20,7 +20,7 @@ hotelName = document.getElementById('hotelName'); //Get elements
 roomRate = document.getElementById('roomRate');
 specialRate = document.getElementById('specialRate');
 
-hotelName.textContent = hotel.Name; //Write hotel name
+hotelName.textContent = hotel.name; //Write hotel name
 roomRate.textContent = '$' + hotel.roomRate.toFixed(2); //Write room rate
 specialRate.textContent = '$' + hotel.offerPrice(); //Write offer price
 
@@ -31,17 +31,22 @@ var elEnds; //The element that shows the message about the offer ending
 
 function offerExpires(today) {
 //Declare variables within the function for local scope
+  
 var weekFromToday, day, date, month, year, dayNames, monthNames;
 //Add 7 days time (added in milliseconds)
+  
 weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
 //Create arrays to hold the names of days/months
+  
 dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  
 //Collect the parts of the date to show on the page
 day = dayNames[weekFromToday.getDay()];
 date = weekFromToday.getDate();
 month = monthNames[weekFromToday.getMonth()];
 year = weekFromToday.getFullYear();
+  
 //Create the message
 expiryMsg = 'Offer expires next ';
 expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
