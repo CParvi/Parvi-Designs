c8eae8a49a1082f131531413d592d9cd009214a1
CParvi <77135493+CParvi@users.noreply.github.com>
2021-02-16 19:06:45 -0800

    Add files via upload

---
+++ Javascript/randomapi.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+    var url = "https://randomuser.me/api/?results=1";
+    var p ="";
+
+
+    fetch(url)
+        .then((response) => response.json())
+        .then(function(data){
+           url = "https://randomuser.me/api/?results=1";
+           fetchInformation(url);
+        })
+
+
+    function fetchInformation(url){
+        fetch(url)
+        .then((response) => response.json())
+        .then(function(data){
+
+           data.results.forEach(person => {
+
+               p = `<div class="well">
+               
+               <span style="margin-left:100px;">${person.name.title}  ${person.name.first} ${person.name.last}</span>
+               <span style="text-transform:italic;">${person.name.title}  ${person.name.first} ${person.name.last}</span>
+               <span style="margin-left:100px;">Email: ${person.email}</span>
+               </div>`;
+               console.log(p);
+               $("#result").append(p);
+               
+           }); 
+
+         
+        
+           
+        })
+    }
+
+
+
+
+});
