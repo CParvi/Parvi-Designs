dd195abbc957d7c5722296cf4f6be2b7c41d4b1e
CParvi <77135493+CParvi@users.noreply.github.com>
2021-02-23 23:13:41 -0800

    Update accordion.js

--- Javascript/accordion.js
+++ Javascript/accordion.js
@@ -1,7 +1,12 @@
-$('.accordion').on('click', '.accordion-control', function(e){ // When clicked
-  e.preventDefault();                    // Prevent default action of button
-  $(this)                                // Get the element the user clicked on
-    .next('.accordion-panel')            // Select following panel 
-    .not(':animated')                    // If it is not currently animating
-    .slideToggle();                      // Use slide toggle to show or hide it
-});
+(function($){	//Use $ as variable name
+	$.fn.accordion = function(speed) {	//Return the jQuery selection
+		this.on('click', '.accordion-control', function(e){
+			e.preventDefault();
+			$(this)
+				.next('.accordion-panel')
+				.not(':animated')
+				.slideToggle(speed);
+		});
+		return this;	//Return the jQuery selection
+	}
+})(jQuery);	//Pass in jQuery object
